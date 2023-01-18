4d956cbc8010bb09ca0159042929d00f516798a8
hamidhosen42 <mdhamidhosen8444@gmail.com>
2023-01-18 09:39:00 +0600

    view

---
+++ lib/utils/routes/routes.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_mvvm_architecture/view/login_view.dart';
+
+import '../../view/home_screen.dart';
+import 'routes_name.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RoutesName.splash:
+      // return MaterialPageRoute(builder: (BuildContext context) => const SplashView());
+
+      case RoutesName.home:
+        return MaterialPageRoute(
+            builder: (BuildContext context) => HomeScreen());
+      case RoutesName.login:
+        return MaterialPageRoute(
+            builder: (BuildContext context) => LoginScreen());
+      case RoutesName.signUp:
+      // return MaterialPageRoute(builder: (BuildContext context) => const SignUpView());
+
+      default:
+        return MaterialPageRoute(builder: (_) {
+          return const Scaffold(
+            body: Center(
+              child: Text('No route defined'),
+            ),
+          );
+        });
+    }
+  }
+}
---
+++ lib/view/home_screen.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
